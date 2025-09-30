8e7663a0277f1fcf446c817f71743b355c1f681f
lukeskywlker19 <thelukeskywlker19@gmail.com>
2025-09-30 17:01:08 -0400

    Update the paper api to 1.21.9, update SkyLib to 1.3.1.0, update the shadow plugin to 9.2.2, and add 1.21.9 support to README.md.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-    compileOnly("com.github.lukesky19:SkyLib:1.3.0.0")
+    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
+    compileOnly("com.github.lukesky19:SkyLib:1.3.1.0")
 
     // Hooks
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
