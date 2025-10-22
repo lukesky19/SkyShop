bd34bcc3590abd7c14b69b9607ed9eb15e9e76ed
lukeskywlker19 <thelukeskywlker19@gmail.com>
2025-10-22 18:00:15 -0400

    Update the paper api to 1.21.10 and add 1.21.10 support to README.md.
    Signed-off-by: lukeskywlker19 <thelukeskywlker19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.9-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
     compileOnly("com.github.lukesky19:SkyLib:1.3.1.0")
 
     // Hooks
